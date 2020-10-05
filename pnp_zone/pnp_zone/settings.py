1dfa734c51229af0ac276abf8f38c11e14a4fc9c
myOmikron <accounts@omikron.dev>
2020-10-05 04:05:13 +0200

    Initialized channels

--- pnp_zone/pnp_zone/settings.py
+++ pnp_zone/pnp_zone/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import channels
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +71,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'pnp_zone.wsgi.application'
+ASGI_APPLICATION = "pnp_zone.routing.application"
 
 
 # Database
