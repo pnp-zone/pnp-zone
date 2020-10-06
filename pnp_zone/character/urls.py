97e336fca87af9f76964c8457bbac6e03e47ee2d
gammelalf <31348226+gammelalf@users.noreply.github.com>
2020-10-06 06:56:41 +0200

    Added simple unstable export feature to character app

--- pnp_zone/character/urls.py
+++ pnp_zone/character/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
+from character.views import *
+
 
 urlpatterns = [
-    path('/', ),
+    path("export", ExportView.as_view())
 ]
--- pnp_zone/character/views.py
+++ pnp_zone/character/views.py
@@ -1,3 +1,15 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+from character.models import ChantModel
+from character import models
 
 
+class ExportView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        cls = models.__dict__[request.GET["class"]]
+        data = []
+        for obj in ChantModel.objects.all():
+            data.append(dict([(key, value) for key, value in obj.__dict__.items() if key != "_state"]))
+        return JsonResponse(data, safe=False)
