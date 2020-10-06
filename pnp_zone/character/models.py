ebbff2377f741994be338dd8a31133d361c5e525
myOmikron <accounts@omikron.dev>
2020-10-06 04:20:08 +0200

    Added __str__ representations, added database options

--- pnp_zone/character/models.py
+++ pnp_zone/character/models.py
@@ -1,48 +1,67 @@
 from django.db import models
-from django.db.models import CharField, DateField, ForeignKey, IntegerField, ManyToManyField
+from django.db.models import CharField, ForeignKey, IntegerField, ManyToManyField
 
 
 class DisadvantageModel(models.Model):
-    name = CharField(max_length=255, null=False)
+    name = CharField(max_length=255)
     description = CharField(max_length=1024, default="")
     adventurer_point_cost = IntegerField()
+
+    def __str__(self):
+        return self.name
 
 
 class AdvantageModel(models.Model):
-    name = CharField(max_length=255, null=False)
+    name = CharField(max_length=255)
     description = CharField(max_length=1024, default="")
     adventurer_point_cost = IntegerField()
 
+    def __str__(self):
+        return self.name
+
 
 class SpeciesModel(models.Model):
-    name = CharField(max_length=255, null=False)
+    name = CharField(max_length=255)
     health_base = IntegerField()
     soul_resistance_base = IntegerField()
     body_resistance_base = IntegerField()
     speed_base = IntegerField()
     ability_max_modifier = CharField(max_length=255, default="")
-    advantages = ManyToManyField(AdvantageModel, on_delete=models.CASCADE)
-    disadvantages = ManyToManyField(DisadvantageModel, on_delete=models.CASCADE)
+    advantages = ManyToManyField(AdvantageModel, blank=True)
+    disadvantages = ManyToManyField(DisadvantageModel, blank=True)
     adventurer_point_cost = IntegerField()
+
+    def __str__(self):
+        return self.name
 
 
 class CultureModel(models.Model):
-    name = CharField(max_length=255, null=False)
+    name = CharField(max_length=255)
     description = CharField(max_length=1024, default="")
+
+    def __str__(self):
+        return self.name
 
 
 class CharacterModel(models.Model):
-    name = CharField(max_length=255, null=False)
-    family = CharField(max_length=255, default="")
-    birth_date = DateField()
-    hair_color = CharField(max_length=255, default="")
-    species = ForeignKey(SpeciesModel, on_delete=models.CASCADE)
-    culture = ForeignKey(CultureModel, on_delete=models.CASCADE)
-    title = CharField(max_length=255, default="")
+    name = CharField(max_length=255)
+    family = CharField(max_length=255, default="", blank=True)
+    birth_date = CharField(max_length=255, default="", blank=True)
+    birth_place = CharField(max_length=255, default="", blank=True)
+    age = IntegerField(default=0, blank=True)
+    hair_color = CharField(max_length=255, default="", blank=True)
+    species = ForeignKey(SpeciesModel, on_delete=models.CASCADE, null=True, blank=True)
+    culture = ForeignKey(CultureModel, on_delete=models.CASCADE, null=True, blank=True)
+    title = CharField(max_length=255, default="", blank=True)
+    characteristics = CharField(max_length=255, default="", blank=True)
+    other = CharField(max_length=255, default="", blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 class SpellModel(models.Model):
-    name = CharField(max_length=255, null=False)
+    name = CharField(max_length=255)
     trial = CharField(max_length=255)
     effect = CharField(max_length=255)
     casting_time = CharField(max_length=255)
@@ -55,10 +74,16 @@ class SpellModel(models.Model):
     leveling_cost = CharField(max_length=1)
     rule_book = CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class SpellExtensionModel(models.Model):
-    _spell = ForeignKey(Model, on_delete=CASCADE)
     name = CharField(max_length=255, null=False)
+    description = CharField(max_length=255)
+    spell = ForeignKey(SpellModel, on_delete=models.CASCADE)
     required_skill_value = IntegerField()
     adventurer_point_cost = IntegerField()
-    description = CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
