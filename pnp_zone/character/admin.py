988f5a2b1f703f00a607c92978b2235a3e403b74
gammelalf <31348226+gammelalf@users.noreply.github.com>
2020-10-08 05:38:33 +0200

    Registered all models on the admin site

--- pnp_zone/character/admin.py
+++ pnp_zone/character/admin.py
@@ -1,15 +1,10 @@
 from django.contrib import admin
-from character.models import *
+from django.apps import apps
 
 
-admin.site.register(DisadvantageModel)
-admin.site.register(AdvantageModel)
-admin.site.register(SpeciesModel)
-admin.site.register(CultureModel)
-admin.site.register(CharacterModel)
-admin.site.register(RulebookModel)
-admin.site.register(ChantModel)
-admin.site.register(CeremonyModel)
-admin.site.register(ChantExtensionModel)
-admin.site.register(CeremonyExtensionModel)
-admin.site.register(BlessingsModel)
+for model in apps.get_models():
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
+
