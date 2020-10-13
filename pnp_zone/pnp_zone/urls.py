19939a0386e449d84150b05843bba93aa84b6861
myOmikron <accounts@omikron.dev>
2020-10-13 02:34:11 +0200

    Changed character urls include to dashboard

--- pnp_zone/pnp_zone/urls.py
+++ pnp_zone/pnp_zone/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-import character.urls
 import dashboard.urls
 
 urlpatterns = [
     path("", include(dashboard.urls)),
     path("admin/", admin.site.urls),
-    path("character/", include(character.urls))
 ]
