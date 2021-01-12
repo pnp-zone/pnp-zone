b0aefde462d7fcb69a41b35bd97211730f063788
gammelalf <31348226+gammelalf@users.noreply.github.com>
2021-01-12 20:25:46 +0100

    Fixed loading order of imports

--- pnp_zone/pnp_zone/asgi.py
+++ pnp_zone/pnp_zone/asgi.py
@@ -9,6 +9,13 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
-from pnp_zone.routing import application
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pnp_zone.settings')
+
+# Fetch Django ASGI application early to ensure AppRegistry is populated
+# before importing consumers and AuthMiddlewareStack that may import ORM
+# models.
+get_asgi_application()
+
+from pnp_zone.routing import application
