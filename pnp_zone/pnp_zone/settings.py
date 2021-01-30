1ad418482e249b57c0aa595b8c27c382d26d05b1
myOmikron <accounts@omikron.dev>
2021-01-30 17:43:30 +0100

    Added login and logout default options

--- pnp_zone/pnp_zone/settings.py
+++ pnp_zone/pnp_zone/settings.py
@@ -131,6 +131,12 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Login / Logout
+LOGIN_URL = "/login"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/login"
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
