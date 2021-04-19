dd4a3d9c63948dcff45f008620631064d99b6e65
myOmikron <accounts@omikron.dev>
2021-04-19 16:19:28 +0200

    Added account model

--- pnp_zone/accounts/models.py
+++ pnp_zone/accounts/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import OneToOneField
 
-# Create your models here.
+
+class AccountModel(models.Model):
+    user = OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
