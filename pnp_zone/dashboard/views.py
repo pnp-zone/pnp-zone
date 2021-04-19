b4cbe7682e7833fcf5cf8d7c12fb66ac1258bc51
myOmikron <accounts@omikron.dev>
2021-04-19 15:31:04 +0200

    Made login required when accessing /

--- pnp_zone/dashboard/views.py
+++ pnp_zone/dashboard/views.py
@@ -1,13 +1,12 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
 from pnp_zone import menu
 
 
-class DashboardView(TemplateView):
+class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = "dashboard/index.html"
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, {"title": "here be dragons",
-                                                    "menu": menu.get("/")})
-
+        return render(request, self.template_name, {"title": "here be dragons", "menu": menu.get("/")})
