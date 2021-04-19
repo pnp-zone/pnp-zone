cd154b71f43c852031fe6eebc5a9661e1d810c37
myOmikron <accounts@omikron.dev>
2021-04-19 16:43:14 +0200

    LoginView creates now accounts if needed

--- pnp_zone/pnp_zone/views.py
+++ pnp_zone/pnp_zone/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView, LogoutView
 
-from pnp_zone import menu
+from accounts.models import AccountModel
 
 
 class Login(LoginView):
@@ -8,6 +9,15 @@ class Login(LoginView):
     extra_context = {
         "title": "Login"
     }
+
+    def get_success_url(self):
+        try:
+            user = User.objects.get(username=self.request.user)
+        except User.DoesNotExist:
+            return False
+        account, created = AccountModel.objects.get_or_create(user=user)
+        account.save()
+        return super().get_success_url()
 
 
 class Logout(LogoutView):
