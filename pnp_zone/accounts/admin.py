6cea81b083a4e93715dec723aa78c63ce57acbd9
myOmikron <accounts@omikron.dev>
2021-04-20 14:32:56 +0200

    Added account to admin view

--- pnp_zone/accounts/admin.py
+++ pnp_zone/accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from accounts.models import *
+
+
+@admin.register(AccountModel)
+class AccountAdmin(ModelAdmin):
+    list_display = ("__str__",)
