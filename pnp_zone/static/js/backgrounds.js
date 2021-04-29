bd10e61c4ba30a2e2d10adb3f62f6d23ab0692f1
gammelalf <31348226+gammelalf@users.noreply.github.com>
2021-04-30 01:08:03 +0200

    Implemented custom element for hitboxes

--- pnp_zone/static/js/backgrounds.js
+++ pnp_zone/static/js/backgrounds.js
@@ -1,8 +1,7 @@
 import tags from "./lib/tagFactory.js";
-import {Drag, LEFT_BUTTON, registerContextMenu} from "./lib/mouse.js";
+import {registerContextMenu} from "./lib/mouse.js";
 import socket from "./socket.js";
-
-const DELTA = 20;
+import "./lib/hitbox.js";
 
 const CONTAINER = document.getElementById("backgrounds");
 const HITBOXES = document.getElementById("backgroundHitboxes");
@@ -59,210 +58,44 @@ export function deleteBackground(event) {
     delete backgrounds[id];
 }
 
-class Hitbox {
-    createChildBox(pos, cursor = "auto") {
-        // parse parameter
-        let ns = pos.match(/[NS]/);
-        if (ns !== null) {
-            ns = ns[0];
-        } else {
-            ns = "";
-        }
-
-        let we = pos.match(/[WE]/);
-        if (we !== null) {
-            we = we[0];
-        } else {
-            we = "";
-        }
-
-        // base definition for div
-        const obj = {
-            class: "board-element",
-            style: {
-                cursor,
-                top: "50%",
-                left: "50%",
-                backgroundColor: "#00f",
-                margin: "" + DELTA + "px",
-                padding: "" + DELTA + "px",
-            }
-        }
-
-        // position
-        if (ns === "N") {
-            obj.style.top = "-" + DELTA + "px";
-        }
-        if (ns === "S") {
-            obj.style.top = "auto";
-            obj.style.bottom = "-" + DELTA + "px";
-        }
-        if (we === "W") {
-            obj.style.left = "-" + DELTA + "px";
-        }
-        if (we === "E") {
-            obj.style.left = "auto";
-            obj.style.right = "-" + DELTA + "px";
-        }
-
-        // drag callback
-        const parent = this.parent;
-        function resize(direction, amount) {
-            switch (direction) {
-                case "N":
-                    parent.y += amount;
-                    parent.height -= amount;
-                    break;
-                case "S":
-                    parent.height += amount;
-                    break;
-                case "W":
-                    parent.x += amount;
-                    parent.width -= amount;
-                    break;
-                case "E":
-                    parent.width += amount;
-                    break;
-            }
-            return parent;
-        }
-
-        let callback;
-        if ((ns !== "") && (we !== "")) {
-            callback = (function (dx, dy, event) {
-                // corner the drag started from
-                const cornerX = parent.x + (we === "E" ? parent.width : 0);
-                const cornerY = parent.y + (ns === "S" ? parent.height : 0);
-
-                // slope of the diagonal through that corner
-                let m;
-                if (((ns === "N") && (we === "W")) || ((ns === "S") && (we === "E"))) {
-                    m = this.ratio;
-                } else {
-                    m = -this.ratio;
-                }
-
-                // is the cursor above that diagonal
-                const above = m * (event.boardX - cornerX) > event.boardY - cornerY;
-
-                let height, width;
-                if ((ns === "N" && !above) || (ns === "S" && above)) {
-                    width = we === "W" ? parent.width - dx : parent.width + dx;
-                    height = width * this.ratio;
-                } else {
-                    height = ns === "N" ? parent.height - dy : parent.height + dy;
-                    width = height / this.ratio;
-                }
-
-                resize(ns, ns === "N" ? parent.height - height : height - parent.height);
-                resize(we, we === "W" ? parent.width - width : width - parent.width);
-            }).bind(this);
-        }
-        else if (ns !== "") {
-            callback = function(dx, dy) { resize(ns, dy); };
-        } else {
-            callback = function(dx, dy) { resize(we, dx); };
-        }
-
-        const hitbox = tags.div(obj);
-        new Drag(this.wrapDragMove(callback), hitbox, LEFT_BUTTON).enable();
-        return hitbox
-    }
-
-    constructor(parent) {
-        this.parent = parent;
-
-        const n = this.createChildBox("N", "ns-resize");
-        const s = this.createChildBox("S", "ns-resize");
-        const w = this.createChildBox("W", "ew-resize");
-        const e = this.createChildBox("E", "ew-resize");
-
-        const nw = this.createChildBox("NW", "nwse-resize");
-        const se = this.createChildBox("SE", "nwse-resize");
-        const ne = this.createChildBox("NE", "nesw-resize");
-        const sw = this.createChildBox("SW", "nesw-resize");
-
-        this.children = {n, s, w, e, nw, se, ne, sw};
-        this.main = tags.div({
-            class: "board-element hitbox",
-            tabIndex: 0,
-            style: {
-                cursor: "move",
-                left: 0,
-                top: 0,
-                padding: "" + DELTA + "px",
-            },
-            children: [
-                w, e, n, s,
-                nw, se, ne, sw
-            ],
-        });
-        new Drag(this.wrapDragMove(function(dx, dy) {
-            parent.x += dx;
-            parent.y += dy;
-        }), this.main, LEFT_BUTTON).enable();
-
-        HITBOXES.appendChild(this.main);
-    }
-
-    wrapDragMove(callback) {
-        let prevX;
-        let prevY;
-        let startX;
-        let startY;
-        let startParent;
-
-        return {
-            dragStart(event) {
-                prevX = event.boardX;
-                prevY = event.boardY;
-                startX = event.boardX;
-                startY = event.boardY;
-                startParent = {x: this.x, y: this.y, width: this.width, height: this.height};
-            },
-            dragMove(event) {
-                const dx = event.boardX - prevX;
-                const dy = event.boardY - prevY;
-                callback(dx, dy, event);
-                prevX = event.boardX;
-                prevY = event.boardY;
-            },
-            dragEnd: function() {
-                this.main.focus();
-                socket.send({type: "background.move", id: this.parent.id, url: this.parent.url,
-                    x: this.parent.x, y: this.parent.y, width: this.parent.width, height: this.parent.height});
-            }.bind(this)
-        };
-    }
-
-    get x() { return getNumericStyle(this.main, "left"); }
-    get y() { return getNumericStyle(this.main, "top"); }
-    set x(value) { setNumericStyle(this.main, "left", value); }
-    set y(value) { setNumericStyle(this.main, "top", value); }
-    get width() { return getNumericStyle(this.main, "width"); }
-    get height() { return getNumericStyle(this.main, "height"); }
-    set width(value) { setNumericStyle(this.main, "width", value); }
-    set height(value) { setNumericStyle(this.main, "height", value); }
-    get ratio() { return this.height / this.width; }
-}
-
 class Background extends HTMLImageElement {
     static observedAttributes = ["x", "y", "width", "height"];
 
     constructor() {
         super();
         if (is_moderator) {
-            this.hitbox = new Hitbox(this);
+            this.hitbox = document.createElement("hitbox-hitbox");
+            HITBOXES.appendChild(this.hitbox);
+
+            // Receive size changes
+            this.hitbox.owner = this;
+
+            // Make focusable
+            this.hitbox.tabIndex = 0;
+
+            // Send new size over socket
+            this.hitbox.sizeChangedCallback = function() {
+                socket.send({type: "background.move", id: this.id, url: this.url,
+                    x: this.x, y: this.y, width: this.width, height: this.height});
+            }.bind(this);
+
+            // Start hidden and appear/disappear with focus
+            this.hitbox.setAttribute("visible", "false");
+            this.hitbox.addEventListener("focus", () => {
+                this.hitbox.setAttribute("visible", "true");
+            });
+            this.hitbox.addEventListener("blur", () => {
+                this.hitbox.setAttribute("visible", "false");
+            });
         }
         this.style.left = "0px";
         this.style.right = "0px";
-        this.style.padding = "" + DELTA + "px";
         this.classList.add("board-element");
     }
 
     connectedCallback() {
         if (is_moderator) {
-            registerContextMenu(this.hitbox.main, () => {
+            registerContextMenu(this.hitbox, () => {
                 return [
                     tags.button({
                         onclick: (event) => {
@@ -272,7 +105,6 @@ class Background extends HTMLImageElement {
                     }),
                 ];
             }).enable();
-
         }
     }
 
