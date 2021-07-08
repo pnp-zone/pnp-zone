06fef3a2d40af7d122f8df5432005a31a58a7471
gammelalf <31348226+gammelalf@users.noreply.github.com>
2021-07-08 18:12:00 +0200

    Refactored top level structure and toggle toolbar via js not css

--- pnp_zone/static/react/main.js
+++ pnp_zone/static/react/main.js
@@ -2,48 +2,55 @@ import React from "https://cdn.skypack.dev/react";
 import ReactDOM from "https://cdn.skypack.dev/react-dom";
 import Board from "./board.js";
 import Moderator from "./moderator.js";
+import CheckBox, {StatefulCheckBox} from "./forms/checkbox.js";
 const e = React.createElement;
 
-class Main extends React.Component {
+function Main(props) {
+    const {x, y, scale, characters, tiles, images} = props;
+    const {isModerator} = props;
+    const [boardView, setBoardView] = React.useState(null);
+    const [viewToolbar, setViewToolbar] = React.useState(false);
 
-    constructor(props) {
-        super(props);
-        this.boardView = null;
-    }
+    const ref = React.useRef();
+    React.useEffect(() => {
+        if (boardView === null) {
+            setBoardView(ref.current);
+        }
+    });
 
-    render() {
-        const {x, y, scale, characters, tiles, images} = this.props;
-        const {isModerator} = this.props;
-        return e(React.Fragment, {}, [
-            e("div", {
-                id: "jitsi",
-                key: "jitsi"
+    return e(React.Fragment, {}, [
+        e("div", {
+            id: "board-view",
+            key: "board",
+            ref,
+        }, [
+            e(Board, {parent: boardView, x, y, scale, characters, tiles, images}),
+        ]),
+        e("div", {
+            id: "jitsi",
+            key: "jitsi"
+        }),
+        ...(isModerator ? [
+            e(CheckBox, {
+                key: "toggle",
+                className: "toggleModerator",
+                value: viewToolbar,
+                setValue: setViewToolbar,
             }),
             e("div", {
-                id: "board-view",
-                key: "board",
-                ref: (node) => {if (this.boardView === null) {this.boardView = node; this.setState({})}},
+                key: "toolbar",
+                id: "moderator",
+                className: "flex-horizontal",
+                style: {
+                    display: viewToolbar ? "" : "none",
+                }
             }, [
-                e(Board, {parent: this.boardView, x, y, scale, characters, tiles, images}),
-            ]),
-            ...(isModerator ? [
-                e("input", {
-                    key: "toggle",
-                    type: "checkbox",
-                    className: "toggleModerator",
+                e(Moderator, {
+                    board: boardView,
                 }),
-                e("div", {
-                    key: "toolbar",
-                    id: "moderator",
-                    className: "flex-horizontal",
-                }, [
-                    e(Moderator, {
-                        board: this.boardView,
-                    }),
-                ]),
-            ] : []),
-        ]);
-    }
+            ]),
+        ] : []),
+    ]);
 }
 
 ReactDOM.render(
