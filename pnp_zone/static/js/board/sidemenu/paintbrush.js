e3ced9f20080b9e9f1c79b26452629c95241c603
gammelalf <31348226+gammelalf@users.noreply.github.com>
2022-02-06 15:48:21 +0100

    Seperated mode dependent drag behaviour from Component

--- pnp_zone/static/js/board/sidemenu/paintbrush.js
+++ pnp_zone/static/js/board/sidemenu/paintbrush.js
@@ -4,18 +4,51 @@ import {LEFT_BUTTON} from "../../lib/mouse.js";
 import socket from "../../socket.js";
 import {Coord, Line} from "../grid.js";
 import {ColorPicker} from "../forms/colorpicker.js";
+import staticUrl from "../../lib/static.js";
 const e = React.createElement;
 
 const Modes = {
     PAINT: "paint",
     ERASE: "erase",
-    NONE: "none",
 };
 const ModeIcons = {
-    [Modes.PAINT]: "/static/img/paintbrush.svg",
-    [Modes.ERASE]: "/static/img/eraser.svg",
-    [Modes.NONE]: null,
+    [Modes.PAINT]: staticUrl("img/paintbrush.svg"),
+    [Modes.ERASE]: staticUrl("img/eraser.svg"),
 };
+
+class PaintDrag extends Drag {
+    constructor(visitCallback) {
+        super();
+        this.register(LEFT_BUTTON, this);
+        this.visitCallback = visitCallback;
+        this.previously = null;
+        this.visited = {};
+    }
+    visit(x, y) {
+        if (isNaN(x) || isNaN(y))
+            return;
+
+        const key = ""+x+" "+y;
+        if (!this.visited.hasOwnProperty(key)) {
+            this.visited[key] = null;
+            this.visitCallback(x, y);
+        }
+    }
+    dragStart(event) {
+        this.previously = Coord.fromIndex(event.nativeEvent.gridX, event.nativeEvent.gridY);
+        this.visit(event.nativeEvent.gridX, event.nativeEvent.gridY);
+    }
+    dragMove(event) {
+        const points = (new Line(this.previously, Coord.fromIndex(event.gridX, event.gridY))).points;
+        for (let i = 0; i < points.length; i++) {
+            this.visit(points[i].xIndex, points[i].yIndex);
+        }
+        this.previously = Coord.fromIndex(event.gridX, event.gridY);
+    }
+    dragEnd() {
+        this.visited = {};
+    }
+}
 
 export default class Paintbrush extends React.PureComponent {
 
@@ -24,78 +57,54 @@ export default class Paintbrush extends React.PureComponent {
         this.state = {
             background: "hsla(0, 0%, 100%, 1)",
             border: "hsla(0, 0%, 0%, 1)",
-            mode: Modes.NONE,
+            mode: null,
             colorPicker: "background",
         };
 
-        this.toSend = [];
+        this.toColor = [];
+        this.toErase = [];
         this.sendTimeout = null;
-        this.visited = {};
-        this.previously = null;  //previously colored Tile
 
-        this.drag = new Drag();
-        this.drag.register(LEFT_BUTTON, this);
+        this.drag = {
+            [Modes.PAINT]: new PaintDrag((x, y) => {
+                this.toColor.push([x, y]);
+                const {background, border} = this.state;
+                socket.sendLocally({type: "layer.set", layer: this.props.layer, key: `${x} ${y}`, object: {x, y, background, border}});
+
+                if (!this.sendTimeout)
+                    this.sendTimeout = setTimeout(this.send.bind(this), 1000);
+            }),
+            [Modes.ERASE]: new PaintDrag((x, y) => {
+                this.toErase.push([x, y]);
+                socket.sendLocally({type: "layer.delete", layer: this.props.layer, key: `${x} ${y}`});
+
+                if (!this.sendTimeout)
+                    this.sendTimeout = setTimeout(this.send.bind(this), 1000);
+            }),
+        };
     }
 
     send() {
-        if (this.state.mode === Modes.PAINT) {
+        if (this.toColor.length > 0) {
             const {background, border} = this.state;
-            socket.send({type: "tiles.color", tiles: this.toSend, background, border, layer: this.props.layer});
-        } else if (this.state.mode === Modes.ERASE) {
-            socket.send({type: "tiles.delete", tiles: this.toSend, layer: this.props.layer});
+            socket.send({type: "tiles.color", tiles: this.toColor, background, border, layer: this.props.layer});
+            this.toColor = [];
         }
-        this.toSend = [];
+        if (this.toErase.length > 0) {
+            socket.send({type: "tiles.delete", tiles: this.toErase, layer: this.props.layer});
+            this.toErase = [];
+        }
         this.sendTimeout = null;
     }
 
-    color(x, y) {
-        if (isNaN(x) || isNaN(y)) {
-            return;
-        }
-
-        const key = ""+x+" "+y;
-        if (!this.visited.hasOwnProperty(key)) {
-            this.visited[key] = null;
-
-            this.toSend.push([x, y]);
-            if (!this.sendTimeout)  {
-                this.sendTimeout = setTimeout(this.send.bind(this), 1000);
-            }
-
-            // Directly write tile to board
-            if (this.state.mode === Modes.PAINT) {
-                const {background, border} = this.state;
-                socket.sendLocally({type: "layer.set", layer: this.props.layer, key: `${x} ${y}`, object: {x, y, background, border}});
-            } else if (this.state.mode === Modes.ERASE) {
-                socket.sendLocally({type: "layer.delete", layer: this.props.layer, key: `${x} ${y}`});
-            }
-        }
-    }
-
-    dragStart(event) {
-        this.previously = Coord.fromIndex(event.nativeEvent.gridX, event.nativeEvent.gridY);
-        this.color(event.nativeEvent.gridX, event.nativeEvent.gridY);
-    }
-    dragMove(event) {
-        const points = (new Line(this.previously, Coord.fromIndex(event.gridX, event.gridY))).points;
-        for (let i = 0; i < points.length; i++) {
-            this.color(points[i].xIndex, points[i].yIndex);
-        }
-        this.previously = Coord.fromIndex(event.gridX, event.gridY);
-    }
-    dragEnd() {
-        this.visited = {};
-    }
-
     componentDidUpdate(prevProps, prevState) {
-        if (this.state.mode !== prevState.mode) {
-            if (this.state.mode === Modes.NONE) {
-                GlobalDrag.removeHandler(this.drag.onMouseDown);
-                document.body.style.cursor = "";
-            } else {
-                GlobalDrag.addHandler(this.drag.onMouseDown);
-                document.body.style.cursor = "crosshair";
-            }
+        if (this.drag[prevState.mode] !== undefined) {
+            document.body.style.cursor = "";
+            GlobalDrag.removeHandler(this.drag[prevState.mode].onMouseDown);
+        }
+        if (this.drag[this.state.mode] !== undefined) {
+            document.body.style.cursor = "crosshair";
+            GlobalDrag.addHandler(this.drag[this.state.mode].onMouseDown);
         }
     }
 
@@ -104,7 +113,7 @@ export default class Paintbrush extends React.PureComponent {
         return e("button", {
             key: `${mode}Mode`,
             className: this.state.mode === mode ? "active" : "",
-            onClick() {setState((state) => ({mode: state.mode === mode ? Modes.NONE : mode}));},
+            onClick() {setState((state) => ({mode: state.mode === mode ? null : mode}));},
         }, [
             e("img", {src: ModeIcons[mode], width: 32, height: 32})
         ]);
@@ -116,8 +125,7 @@ export default class Paintbrush extends React.PureComponent {
         return e("div", {
             className: "margin",
         }, [
-            this.renderModeSwitch(Modes.PAINT),
-            this.renderModeSwitch(Modes.ERASE),
+            Object.values(Modes).map(this.renderModeSwitch.bind(this)),
             e("div", {
                 className: "flex-horizontal",
             }, [
