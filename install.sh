807fb032df5212aceaf68a0437330242054b4aff
gammelalf <31348226+gammelalf@users.noreply.github.com>
2022-03-02 16:16:52 +0100

    Added mysql to install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-apt install python3-pip git build-essential python3-dev libldap2-dev libsasl2-dev ldap-utils tox lcov valgrind ansible
+apt install python3-pip git build-essential python3-dev libldap2-dev libsasl2-dev ldap-utils tox lcov valgrind ansible mariadb-server mariadb-client default-libmysqlclient-dev
 ansible-playbook install.yml
