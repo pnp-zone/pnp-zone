44ac27c5456d474657d9c894644e8554e52aaedb
myOmikron <git@omikron.dev>
2022-05-05 20:10:30 +0200

    Removed import cycle

--- server/router.go
+++ server/router.go
@@ -4,15 +4,10 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/myOmikron/echotools/color"
 	"github.com/pnp-zone/common/conf"
-	"github.com/pnp-zone/pnp-zone/handler"
 	"gorm.io/gorm"
 )
 
 func defineRoutes(e *echo.Echo, db *gorm.DB, config *conf.Config, plugins []*Plugin) {
-	w := handler.Wrapper{Plugins: plugins}
-
-	e.GET("/", w.Index)
-
 	for _, plugin := range plugins {
 		if err := plugin.RouterHook(e, db, config); err != nil {
 			color.Printf(color.RED, "Plugin %s: Error while defining routes: %s\n", plugin.Name, err.Error())
